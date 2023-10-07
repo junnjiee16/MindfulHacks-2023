9b46d571069b2ee801a9561f45d0636132680709
timothyckl <timothy.ckl@outlook.com>
2023-10-08 07:22:15 +0800

    yes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,21 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Flex, Center, AbsoluteCenter } from "@chakra-ui/react"
 
 export function HomePage() {
     return (
-        <Box>
-            Hello
-        </Box>
+        <>
+            <Flex direction={"column"} height={"100vh"} width={"100vw"} justify={"center"}>
+                OwO
+                <Box mx={"auto"} fontSize={"4xl"} mb={"40px"} bg={"tomato"} width={"600px"} height={"100px"} textAlign={"center"} >
+                    Chatbot
+                </Box>
+                <Box mx={"auto"} fontSize={"4xl"} mb={"40px"} bg={"tomato"} width={"600px"} height={"100px"} textAlign={"center"} >
+                    To-Do List
+                </Box>
+                <Box mx={"auto"} fontSize={"4xl"} mb={"40px"} bg={"tomato"} width={"600px"} height={"100px"} textAlign={"center"} >
+                    Journal
+                </Box>
+            </Flex>
+            
+        </>
     )
 }
