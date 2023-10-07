dd61cf3dfae1c55940f59da5d91e848cfeeb9714
shayaanak32 <destinedfrsuccess09@gmail.com>
2023-10-08 04:56:09 +0800

    failed integration of the card focus feature

--- frontend/src/components/TaskListing.jsx
+++ frontend/src/components/TaskListing.jsx
@@ -2,19 +2,18 @@ import { useState } from "react"
 import { Box, Flex, Spacer, Checkbox, Center, Button } from "@chakra-ui/react"
 import { textToEmoji } from "../apiCalls"
 
-export function TaskListing({ title, duration }) {
+export function TaskListing({ key, title, duration, selected, display, onClick }) {
+    console.log(`Selection: ${selected}`);
     return (
         <Flex p="10px">
-            <Checkbox></Checkbox>
             <Flex direction={"column"} ms="1.3em" rowGap={"0.3em"}>
                 <Box fontWeight={"bold"} fontSize={"1.1em"}>{title}</Box>
                 <Box color={"gray"} fontSize={"0.9em"}>Duration: {duration}</Box>
-
             </Flex>
 
             <Button onClick={() => {
                 console.log('Hi there!');
-            }} ml = "200px">Start Focus</Button>
+            }} ml="200px">Start Focus</Button>
         </Flex>
     )
 }
--- frontend/src/pages/TaskPlannerPage.jsx
+++ frontend/src/pages/TaskPlannerPage.jsx
@@ -4,26 +4,42 @@ import {
     Button,
     Menu,
     MenuButton,
-    MenuList
+    MenuList,
+    ChakraProvider
 } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons"
 import { TaskListing } from "../components/TaskListing";
 import { CreateTaskModal } from "../components/CreateTaskModal";
+import { useState } from "react"
 
 export function TaskPlannerPage() {
+    const [focusedCard, setFocusedCard] = useState(0);
+
+    const cards = [
+        { id: 1, title: 'Hello Task List 1', duration: '1h 30m', selected: false, display: true },
+        { id: 2, title: 'Hello Task List 2', duration: '1h 30m', selected: false, display: true },
+        { id: 3, title: 'Hello Task List 3', duration: '1h 30m', selected: false, display: true },
+        { id: 4, title: 'Hello Task List 4', duration: '1h 30m', selected: false, display: true },
+    ];
+
+    const handleCardClick = (cardId) => {
+        setFocusedCard(cardId);
+    };
     return (
-        <Box p = "10px">
+        <Box p="10px">
             <Flex columnGap={"2em"}>
                 <Box fontWeight={"bold"} fontSize={"1.6em"} mb="1.4em">Planner</Box>
                 <CreateTaskModal />
 
             </Flex>
-
-            <TaskListing title="Hello Task List" duration="1h 30m" />
-            <TaskListing title="Hello Task List" duration="1h 30m" />
-            <TaskListing title="Hello Task List" duration="1h 30m" />
-            <TaskListing title="Hello Task List" duration="1h 30m" />
-
+            {/* <ChakraProvider> */}
+                <TaskListing title="Hello Task List 1" duration="1h 30m" />
+                <TaskListing title="Hello Task List 2" duration="1h 30m" />
+                <TaskListing title="Hello Task List 3" duration="1h 30m" />
+                <TaskListing title="Hello Task List 4" duration="1h 30m" />
+             
+            {/* </ChakraProvider> */}
+          
         </Box>
     )
 }
